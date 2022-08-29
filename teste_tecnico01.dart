da6cead344956d0e4e9db80a38dcacfceefa8e06
robertoalvarezjunior <robertojr.colorado@gmail.com>
2022-08-29 15:50:26 -0300

    first commit

---
+++ teste_tecnico01.dart
@@ -0,0 +1,28 @@
+/*
+  implemente uma função que receba um numero inteiro positivo e retorne o somatorio de todos os valores inteiros divisiveis por 3 ou 5 que sejam inferiores ao numero passado
+ */
+
+void main() {
+  fun();
+}
+
+fun() {
+  int num = 11;
+  int i;
+  var lista = [0];
+  var soma = 0;
+
+  for (i = 0; i < num; i++) {
+    if ((i % 3) == 0 || (i % 5) == 0) {
+      lista.add(i);
+    }
+  }
+  lista.removeAt(0);
+  print(lista);
+
+  for (i = 0; i < lista.length; i++) {
+    soma += lista[i];
+  }
+
+  print(soma);
+}
